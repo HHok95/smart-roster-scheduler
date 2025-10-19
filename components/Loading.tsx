591f83ad54d904b89ee126c0e9be78a02e4f4a8a
HHok95 <S3936933@student.rmit.edu.au>
2025-10-20 01:19:59 +1100

    fix: build-failure-3

---
+++ components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="min-w-[280px] h-[40px] flex items-center justify-center bg-gray-50 animate-pulse">
+      Loading...
+    </div>
+  );
+};
+
+export default loading;
